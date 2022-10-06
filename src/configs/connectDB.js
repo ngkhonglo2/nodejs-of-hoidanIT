ad4647f2cf7c0d249b06c0cba86651d7ea4ca96f
phongnt <bongdem30101998@gmail.com>
2022-10-06 09:36:29 +0700

    complete example of node js

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,18 +1,10 @@
-import mysql from 'mysql2'
+import mysql from 'mysql2/promise'
 
 // create the connection to database
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'nodejsbasic'
 });
 
-// simple query
-connection.query(
-    'SELECT * FROM `users`',
-    function (err, results, fields) {
-        console.log(results); // results contains rows returned by server
-    }
-);
-
-export default connection;
+export default pool;
--- src/server.js
+++ src/server.js
@@ -2,16 +2,32 @@
 import express from "express"
 import configViewEngine from "./configs/viewEngine"
 import initWebRoute from "./router/web"
-import connection from "./configs/connectDB"
+import initAPIRoute from "./router/api"
+// import connection from "./configs/connectDB"
 require('dotenv').config()
+var morgan = require('morgan')
 
 const app = express()
 const port = process.env.PORT || 8080
+
+app.use(morgan('combined'))
+
+// cấu hình đối với các yêu cầu POST và PUT, khi ta đang sending data tới server và ta đang yêu cầu máy chủ chấp nhận hoặc lưu trữ dữ liệu đó
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // setup view engine
 configViewEngine(app)
 // init web route
 initWebRoute(app)
+
+// init api route
+initAPIRoute(app)
+
+// handle 404 not found
+app.use((req, res) => {
+    return res.render('404.ejs')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
